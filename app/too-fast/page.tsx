2ea03d90a66532299a0b62b760523f1949ddbd4d
hakimxiao <alhakimabusolihin@gmail.com>
2026-03-06 00:28:30 +0700

    # Tambah Rate Limiter dan Halaman "Too Fast"
    ## Ringkasan Perubahan
    Implementasi pembatasan akses berdasarkan IP dengan Upstash Ratelimit dan penanganan kasus akses berlebih.
    
    ## Detail Perubahan
    - Tambah file `ratelimit.ts` untuk konfigurasi pembatasan akses.
    - Tambah integrasi Ratelimit di metode `signInWithCredentials` dan `signUp` di `lib/actions/auth.ts`.
    - Tambah halaman `too-fast/page.tsx` untuk menampilkan pesan akses berlebih.
    - Tambah dependensi `@upstash/ratelimit` dan `@upstash/redis` di `package.json`.
    - Perbarui file `config.ts` untuk menambahkan variabel lingkungan Upstash Redis.
    
    ## Alasan Perubahan
    Meningkatkan keamanan dengan mencegah penyalahgunaan sistem melalui pembatasan akses.
    
    ## Dampak
    - Sistem kini memiliki mekanisme pembatasan akses berbasis IP.
    - Tidak ada breaking change.

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="root-container flex min-h-screen flex-col items-center justify-center">
+      <h1 className="font-bebas-neue text-5xl font-bold text-light-100">
+        Whoa, Slown down There, speedy!
+      </h1>
+      <p className="mt-3 max-w-xl text-center text-light-400">
+        Looks like you&apos;ve a little too eager, we&apos;ve put a temporary
+        pause on your !excitment. i chill for a bit, and try again shortly
+      </p>
+    </main>
+  );
+};
+export default Page;
